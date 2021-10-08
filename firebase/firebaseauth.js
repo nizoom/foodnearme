ea83bbd620591ad4906eff60e39f04d7a21eab18
Nissim Ram <nissimram1812@gmail.com>
2021-10-08 00:17:19 -0400

    improved validation for forms // now dynamic to use same function for both form pages

--- firebase/firebaseauth.js
+++ firebase/firebaseauth.js
@@ -1,6 +1,21 @@
+import { getApp } from "@firebase/app";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
+export function signIn(email, password){
+  const auth = getAuth();
 
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+
+}
 export function signUp(email, password){
 
     const auth = getAuth();
@@ -19,40 +34,45 @@ export function signUp(email, password){
 
 
 export function validateForm(email, pw, cfmpw){
+ 
   let error = {
-    emptyString : false,
-    pwMismatch : false,
-    invalidEmail: false,
-    invalidPw: false,
+    emptyString : [false, "Please make sure all fields are filled in"], 
+    pwMismatch : [false, "These passwords don't match"],
+    invalidEmail: [false, "Please enter a valid email"],
+    invalidPw: [false, "Password must be longer than 4 characters"],
   }
   //console.log(arguments)
+
   //empty str
   Array.from(arguments).forEach(arg => {
     if(arg.length < 1){
-      error.emptyString = true;
+      error.emptyString[0] = true;
     }
   })
   //pwMismatch
-   if(pw!==cfmpw){
-     error.pwMismatch = true;
-   } 
+  if(cfmpw !== "..."){ // signup form requires cfmpw
+    if(pw!==cfmpw){
+      error.pwMismatch[0] = true;
+    } 
+  }
+   
    //invalid email
    if(!email.includes("@") || !email.includes(".")
    ||email.length < 5){
-      error.invalidEmail = true;
+      error.invalidEmail[0] = true;
    }
 
    if(pw.length < 5){
-     error.invalidPw = true;
+     error.invalidPw[0] = true;
    }
 
    //return errs that are true
    let errsList = ""
    for(const property in error){
-     if(error[property]){
+     if(error[property][0]){
        //add to list of errors that're true / actually happened
       // errsList.push(property)
-      errsList += `and ${property}`
+      errsList += `and ${error[property][1]} `
      }
    }
    console.log(errsList)
--- pages/signin.js
+++ pages/signin.js
@@ -1,25 +1,43 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import auth from "../firebase/clientApp";
 import Link from "next/link"
-
-
-
+import { signIn } from "../firebase/firebaseauth";
+import { validateForm } from "../firebase/firebaseauth";
 
 function SignInScreen() {
+
+  const emailRef = useRef();
+  const pwRef = useRef();
+
+  const [showErrs, setShowErrs] = useState("");
+
+  const handleSignin = async (event) => {
+    event.preventDefault();
+    const [emailValue, pwValue] = [emailRef.current.value, pwRef.current.value]
+    const possibleErrs = validateForm(emailValue, pwValue, "...") //no pw confirmation here -> blank last arguement
+    if(possibleErrs.length === 0 ){
+      //signIn(emailValue, pwValue);
+    } else {
+      let trimmedErrs = possibleErrs.substr(4)
+      setShowErrs(trimmedErrs)
+    }
+  }
+
     return (
       <div className="signin-page-wrapper">
         
-        <h2> Please sign in</h2>
+        <h1> Please sign in</h1>
         <div className = "signin-wrapper">
-          <form>
+          {showErrs.length > 0 ? <p> {showErrs} </p> : null}
+          <form onSubmit = {handleSignin}>
             <div className="group"> 
               <label htmlFor="email"> Email :</label>
-              <input type = "email" name="email"/> 
+              <input type = "email" name="email" ref = {emailRef}/> 
             </div>
             
             <div className="group"> 
               <label htmlFor="password"> Password </label>
-              <input type="password" name="password"/> 
+              <input type="password" name="password" ref={pwRef}/> 
             </div>
             <button type = "submit"> Submit </button>
             <div>
