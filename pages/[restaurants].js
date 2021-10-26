19af40c6b2bdbc8a32cf357e50e1ff56c517af47
Nissim Ram <nissimram1812@gmail.com>
2021-10-26 16:56:25 -0400

    removed api key

--- pages/[restaurants].js
+++ pages/[restaurants].js
@@ -22,9 +22,7 @@ export async function getServerSideProps(context) {
     }
   }
   
-    //${process.env.NEXT_PUBLIC_GOOGLE_KEY}
-    //AIzaSyCjYaPndbfufJc-FoD7jZhA_FWa85FP2QA
-  
+ 
 
 const DisplayRestaurants = ({ restaurants, context }) => {
     console.log(context)
@@ -45,17 +43,3 @@ const DisplayRestaurants = ({ restaurants, context }) => {
 }
 
 export default DisplayRestaurants;
-
-// export async function findFood(coords, key, keywords){
-//     console.log('finding places')
-//     console.log(arguments)
-//     //const url= `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522%2C151.1957362&radius=1500&type=restaurant&keyword=cruise&key=YOUR_API_KEY`
-//     const url= `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coords[0]},${coords[1]}&radius=1500&type=restaurant&keyword=cruise&key=${key}`
-//     const response = await fetch(url, {
-//         mode : 'cors',
-//         headers : { 'Access-Control-Allow-Origin':'*' }
-//     })
-//         .then(
-//             response => response.json()
-//         );
-// }
